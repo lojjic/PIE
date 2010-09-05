3f4f0005c7bef636def6352b4f03f92a0e7b0214
Jason Johnston <github@lojjic.net>
2010-09-05 10:13:49 -0600

    Return null object if parsing of -pie-background returns no colors or images

--- sources/BackgroundStyleInfo.js
+++ sources/BackgroundStyleInfo.js
@@ -232,7 +232,7 @@ PIE.BackgroundStyleInfo = PIE.StyleInfoBase.newStyleInfo( {
             } );
         }
 
-        return props;
+        return ( props && ( props.color || props.images[0] ) ) ? props : null;
     },
 
     /**
