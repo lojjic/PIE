cf7f20784ac8206b9513890667a932aa6745f2e5
Jason Johnston <jjohnston@jjohnston.local>
2011-06-19 10:15:20 -0600

    Add support for text-shadow on nested mixed markup. Handle text-indent and inline elements that don't start at the beginning of the line.

--- sources/TextShadowRenderer.js
+++ sources/TextShadowRenderer.js
@@ -20,6 +20,15 @@ PIE.TextShadowRenderer = PIE.RendererBase.newRenderer( {
         return textShadowInfo.isActive() && textShadowInfo.getProps();
     },
 
+    copyTextStyles: function( from, to ) {
+        var fromStyle = from.currentStyle,
+            toStyle = to.style,
+            textStyles = this.textStyles,
+            i = textStyles.length;
+        while( i-- ) {
+            toStyle[ textStyles[ i ] ] = fromStyle[ textStyles[ i ] ];
+        }
+    },
 
     draw: function() {
         var el = this.targetElement,
@@ -28,17 +37,25 @@ PIE.TextShadowRenderer = PIE.RendererBase.newRenderer( {
             si = this.styleInfos,
             shadowProps = si.textShadowInfo.getProps(),
             i = shadowProps.length,
-            textStyles = this.textStyles,
             getLength = PIE.getLength,
             bounds = this.boundsInfo.getBounds(),
             borderProps = si.borderInfo.getProps(),
             borderWidths = borderProps && borderProps.widths,
             borderTopWidth = borderWidths ? borderWidths['t'].pixels( el ) : 0,
             borderLeftWidth = borderWidths ? borderWidths['l'].pixels( el ) : 0,
+            borderRightWidth = borderWidths ? borderWidths['r'].pixels( el ) : 0,
+            paddingLeft = getLength( currentStyle.paddingLeft ).pixels( el ),
+            paddingRight = getLength( currentStyle.paddingRight ).pixels( el ),
+            paddingTop = getLength( currentStyle.paddingTop ).pixels( el ),
+            indent = currentStyle.display === 'block' ?
+                    getLength( currentStyle.textIndent ).pixels( el ) :
+                    currentStyle.display === 'inline' ? el.getClientRects()[0].left - bounds.x : 0,
             filterPrefix = 'progid:DXImageTransform.Microsoft.',
             math = Math,
             round = math.round,
-            props, shadowEl, shadowStyle, color, blur, blurLog, glow, opacity, j;
+            childNodes = el.childNodes,
+            childCount = childNodes.length,
+            props, shadowEl, shadowStyle, color, blur, blurLog, glow, opacity, j, child, clone;
 
         box.innerHTML = '';
         
@@ -73,24 +90,67 @@ PIE.TextShadowRenderer = PIE.RendererBase.newRenderer( {
                                      filterPrefix + 'Blur(PixelRadius=' + ( blur - glow ) + ',MakeShadow=false)';
             }
 
+            // Size and position the shadow element
             shadowStyle.position = 'absolute';
-            shadowStyle.left = props.xOffset.pixels( el ) - round(blur) + borderLeftWidth +
-                               getLength( currentStyle.paddingLeft ).pixels( el );
-            shadowStyle.top = props.yOffset.pixels( el ) - round(blur) + borderTopWidth +
-                              getLength( currentStyle.paddingTop ).pixels( el );
-            shadowStyle.width = bounds.w;
-            shadowStyle.height = bounds.h;
+            shadowStyle.left = props.xOffset.pixels( el ) - round(blur) + borderLeftWidth + paddingLeft;
+            shadowStyle.top = props.yOffset.pixels( el ) - round(blur) + borderTopWidth + paddingTop;
+            shadowStyle.width = bounds.w - paddingLeft - paddingRight - borderLeftWidth - borderRightWidth + round(blur) * 2;
+
+            // Handle indentation of the first line, either due to text-indent CSS on a block element
+            // or an inline element that is not the first text on the line and wraps to a second line.
+            shadowStyle.textIndent = indent;
 
             // Copy the original text's styles to the shadow element
-            j = textStyles.length;
-            while( j-- ) {
-                shadowStyle[ textStyles[ j ] ] = currentStyle[ textStyles[ j ] ];
-            }
+            this.copyTextStyles( el, shadowEl );
 
             shadowStyle.color = color.colorValue( el );
-            shadowEl.innerText = el.innerText;
+
+            if ( el.tagName.toLowerCase() in { input:1, textarea:1 } ) {
+                shadowEl.innerText = el.value;
+            } else {
+                var childElements = [];
+                for( j = 0; j < childCount; j++ ) {
+                    child = childNodes[ j ];
+                    if ( child.nodeType === 1 && child.tagName.toLowerCase() !== 'css3-container' ) {
+                        clone = this.deepCopy( child );
+                        clone.style.visibility = 'hidden';
+                        shadowEl.appendChild( clone );
+
+                        clone.style.zoom = 1; //The original child is going to get layout by attaching PIE, so force layout on the clone to match
+                        childElements.push( child );
+                    }
+                    else if ( child.nodeType === 3 ) {
+                        shadowEl.appendChild( child.cloneNode() );
+                    }
+                }
+
+                for( j = childElements.length; j--; ) {
+                    PIE.attach( childElements[ j ] );
+                }
+            }
+
             box.appendChild( shadowEl );
         }
+    },
+
+    deepCopy: function( el ) {
+        var copy = doc.createElement( 'subel' ),
+            childNodes = el.childNodes,
+            childLength = childNodes.length,
+            i = 0, child, nodeType;
+        this.copyTextStyles( el, copy );
+        for ( ; i < childLength; i++ ) {
+            child = childNodes[ i ];
+            nodeType = child.nodeType;
+            if ( nodeType == 1 && child.tagName !== 'css3-container' ) {
+                copy.appendChild( this.deepCopy( child ) );
+            }
+            else if ( nodeType == 3 ) {
+                copy.appendChild( child.cloneNode() );
+            }
+        }
+        copy.style.display = el.currentStyle.display;
+        return copy;
     }
 
 } );
