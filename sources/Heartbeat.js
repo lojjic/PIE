6744f0440f4656cfe19f86d57e4e221c2eb2a51c
Jason Johnston <jason@css3pie.com>
2012-02-07 20:43:15 -0700

    Tweaks to polling heartbeat: use setTimeout rather than setInterval to prevent backing up. Allow setting html{-pie-poll-interval:N} to customize the interval on a page.

--- sources/Heartbeat.js
+++ sources/Heartbeat.js
@@ -2,15 +2,20 @@
  * Simple heartbeat timer - this is a brute-force workaround for syncing issues caused by IE not
  * always firing the onmove and onresize events when elements are moved or resized. We check a few
  * times every second to make sure the elements have the correct position and size. See Element.js
- * which adds heartbeat listeners based on the custom -pie-poll flag, which defaults to true in IE8
+ * which adds heartbeat listeners based on the custom -pie-poll flag, which defaults to true in IE8-9
  * and false elsewhere.
  */
 
 PIE.Heartbeat = new PIE.Observable();
 PIE.Heartbeat.run = function() {
-    var me = this;
+    var me = this,
+        interval;
     if( !me.running ) {
-        setInterval( function() { me.fire() }, 250 );
+        interval = doc.documentElement.currentStyle.getAttribute( PIE.CSS_PREFIX + 'poll-interval' ) || 250;
+        (function beat() {
+            me.fire();
+            setTimeout(beat, interval);
+        })();
         me.running = 1;
     }
 };
